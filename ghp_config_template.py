ba263d80f43aa0a1405fb4d77d382479a1c3ad6d
sergeymalamen <sergey.malamen@gmail.com>
2025-12-10 10:54:29 +0100

    New

---
+++ ghp_config_template.py
@@ -0,0 +1,7 @@
+# Example, not used directly by script - options loaded from /data/options.json
+SERIAL_PORT = "/dev/ttyUSB0"
+MQTT_BROKER = "homeassistant"
+MQTT_PORT = 1883
+MQTT_USERNAME = ""
+MQTT_PASSWORD = ""
+MQTT_PREFIX = "ghp08"
--- ghp_mm_sniffer/run.sh
+++ ghp_mm_sniffer/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/with-contenv bash
-python3 /usr/src/app/ghp_mm2mqtt.py
+python3 -u /usr/src/app/ghp_sniffer.py
